4c8db0790510aa7f8e3f033dc1a0dc318f53f760
furqank729 <58474453+furqankhalid729@users.noreply.github.com>
2024-12-23 22:02:32 +0500

    Local Changes

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BannerWithTabs from "../components/home/BannerWithTabs";
+import FeaturedFleets from "../components/home/FeaturedFleets";
+import FeatureOffers from "../components/home/FeatureOffers";
+import FeatureDestination from "../components/home/FeatureDestination";
+import Testimonials from "../components/home/Testimonials";
+import Gallery from "../components/home/Gallery";
+import Blog from "../components/home/Blog";
+
+const Home = () => {
+  return (
+    <>
+      <BannerWithTabs />
+      <FeaturedFleets />
+      <FeatureOffers/>
+      <FeatureDestination/>
+      <Gallery/>
+      <Blog/>
+      <Testimonials/>
+
+    </>
+  );
+};
+
+export default Home;
