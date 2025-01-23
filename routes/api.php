b05a078b804fe113da93136c16778dcd29a98804
furqank729 <58474453+furqankhalid729@users.noreply.github.com>
2025-01-23 17:28:27 +0500

    Controllers

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/car/add-car', [CarController::class, 'create']);
